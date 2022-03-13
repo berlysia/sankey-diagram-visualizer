5d52418a825ed8f75a6c87767f7cb07743e18c57
berlysia <berlysia@gmail.com>
2022-03-13 22:11:22 +0900

    use nivo

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,5 @@
 import React, { useState } from "react";
-import { Chart } from "react-google-charts";
-
-const options = {
-  sankey: {
-    node: {
-      label: {
-        fontName: "sans-serif",
-        fontSize: 16,
-        color: "#222",
-        bold: true,
-        italic: false,
-      },
-    },
-  },
-};
+import { ResponsiveSankey } from "@nivo/sankey";
 
 const defaultValue = `Brazil,Portugal,5
 Brazil,France,1
@@ -63,33 +49,25 @@ Morocco,India,1
 Morocco,Japan,3
 `;
 
-const HEAD = [
-  "From",
-  "To",
-  "Weight",
-  { type: "string", role: "style" },
-] as const;
-type Head = typeof HEAD;
-type Row = [string, string, number, string];
-type Data = [Head, ...Row[]];
-
+type Data = React.ComponentProps<typeof ResponsiveSankey>["data"];
 function useTransform(seed: string): Data {
-  return [
-    HEAD,
-    ...seed
-      .trim()
-      .split("\n")
-      .map((row) => row.split(/(?:,|\t)/))
-      .map(
-        (row) =>
-          [
-            row[0],
-            row[1],
-            parseFloat(row[2]),
-            row[3]?.toLowerCase() === "true" ? "color: red" : "",
-          ] as Row
-      ),
-  ];
+  const rows = seed
+    .trim()
+    .split("\n")
+    .map((row) => row.split(/(?:,|\t)/));
+  return {
+    nodes: rows
+      .flatMap((x) => [x[0], x[1]])
+      .reduce((a, x) => (a.includes(x) ? a : a.concat(x)), [] as string[])
+      .map((id) => ({ id })),
+    links: rows.map((row) => ({
+      source: row[0],
+      target: row[1],
+      value: parseFloat(row[2]),
+      startColor: row[3]?.toLowerCase() === "true" ? "#f00" : undefined,
+      endColor: row[3]?.toLowerCase() === "true" ? "#f00" : undefined,
+    })),
+  };
 }
 
 function App() {
@@ -105,13 +83,56 @@ function App() {
           onChange={(e) => setData(e.currentTarget.value)}
         ></textarea>
       </div>
-      <Chart
-        chartType="Sankey"
-        width="100%"
-        height="640px"
-        data={data}
-        options={options}
-      />
+      <div style={{ width: "100%", height: "640px" }}>
+        <ResponsiveSankey
+          data={data}
+          margin={{ top: 40, right: 160, bottom: 40, left: 50 }}
+          align="justify"
+          colors={{ scheme: "category10" }}
+          nodeOpacity={1}
+          nodeHoverOthersOpacity={0.35}
+          nodeThickness={18}
+          nodeSpacing={24}
+          nodeBorderWidth={0}
+          nodeBorderColor={{
+            from: "color",
+            modifiers: [["darker", 0.8]],
+          }}
+          nodeBorderRadius={3}
+          linkOpacity={0.5}
+          linkHoverOthersOpacity={0.1}
+          linkContract={3}
+          enableLinkGradient
+          labelPosition="outside"
+          labelOrientation="vertical"
+          labelPadding={16}
+          labelTextColor={{
+            from: "color",
+            modifiers: [["darker", 1]],
+          }}
+          legends={[
+            {
+              anchor: "bottom-right",
+              direction: "column",
+              translateX: 130,
+              itemWidth: 100,
+              itemHeight: 14,
+              itemDirection: "right-to-left",
+              itemsSpacing: 2,
+              itemTextColor: "#999",
+              symbolSize: 14,
+              effects: [
+                {
+                  on: "hover",
+                  style: {
+                    itemTextColor: "#000",
+                  },
+                },
+              ],
+            },
+          ]}
+        />
+      </div>
     </div>
   );
 }
